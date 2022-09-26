f2a06e74df622d154ce3950b8b3e586861e6f48b
CoderDgx <1181571719@qq.com>
2022-09-26 17:52:13 +0800

    feat: 搭建项目规范

---
+++ components/layout/BaseLayout.tsx
@@ -0,0 +1,23 @@
+/*
+ * @Author: CoderDgx 1181571719@qq.com
+ * @Date: 2022-09-26 17:18:44
+ * @LastEditors: CoderDgx 1181571719@qq.com
+ * @LastEditTime: 2022-09-26 17:32:19
+ * @FilePath: /nft-market/components/layout/BaseLayout.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { FC } from "react";
+
+const BaseLayout: FC<
+  React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+> = ({ children }) => {
+  return (
+    <div className="py-16 bg-gray-50 overflow-hidden min-h-screen">
+      <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default BaseLayout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/*
+ * @Author: CoderDgx 1181571719@qq.com
+ * @Date: 2022-09-26 03:35:05
+ * @LastEditors: CoderDgx 1181571719@qq.com
+ * @LastEditTime: 2022-09-26 16:57:46
+ * @FilePath: /nft-market/pages/_app.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { NavBar } from "../components";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
